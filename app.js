12883fee269982a234f519af996969869621680e
mostafizur99 <coder.mr11@gmail.com>
2022-10-28 02:56:49 +0600

    feat: product model added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const express = require('express')
 require('dotenv').config()
-const connectDb = require('./connectDb')
+const connectDb = require('./utils/connectDb')
 
-const connectDB = async () => {
-    try {
-        await connectDb.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-connectDB()
-console.log('Hello')
+const app = express()
+const port = 5000
+
+connectDb.sync()
+    .then(result => {
+        // console.log(result);
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
